8638b893858c1bb9ac79944105af62f29364251f
Vanessa <vanessabizzell@gmail.com>
2025-01-03 20:53:26 +0000

    cipher decomposition start

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function enDeCode(){
     let alphabet = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x", "y","z"];
     let letter = alphabet[index];
     return letter;
+    
   }
   
   function convertLetterToIndex(letter){
